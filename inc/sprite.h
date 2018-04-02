f3b3aa1bff5654465b11c8daa16ca614c1dcb3d7
Chris <cms@dreaming.org>
2018-04-01 19:16:53 -0700

    Sprite class header scaffolding
    Basic data structures to relate bitmap data to visually representative metadata as a component of a graphical display subsystem.

---
+++ inc/sprite.h
@@ -0,0 +1,16 @@
+#ifndef SPRITE_H_
+
+#include <stdio.h>
+
+struct sprite_obj
+{
+	uint16_t width;
+	uint16_t height;
+
+	static const *bmp_data;
+	uint16_t length;
+	
+}
+
+#define SPRITE_H_ 1
+#endif
